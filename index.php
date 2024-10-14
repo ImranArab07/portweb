5a0d950a1877e4d3b8f357324902155122565386
Imran Arab <Imran.a07@hotmail.com>
2024-10-14 14:04:20 +0200

    versie 1

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+// Database inloggegevens
+$host = "localhost";
+$dbname = "profileapp";
+$username = "root";
+$password = "";
+
+$message = "";
+
+try {
+    // Nieuwe PDO-Verbinding maken
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully<br>";
+
+    // Controleer of het formulier via een POST-verzoek is ingediend
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Haal de gegevens op uit het formulier
+        $title = $_POST['title'];
+        $content = $_POST['content'];
+        $author = $_POST['author'];
+
+        // SQL-query om gegevens in te voegen met voorbereide statements
+        $sql = "INSERT INTO posts (title, content, author) VALUES (:title, :content, :author)";
+
+        $stmt = $conn->prepare($sql);
+
+        // Koppel de waarden aan de parameters
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->bindParam(':author', $author);
+
+        // Voer de query uit
+        $stmt->execute();
+
+        // Succesbericht
+        echo "Nieuwe post succesvol aangemaakt!";
+    }
+
+} catch (PDOException $e) {
+    echo "Fout bij het verbinden met de database: " . $e->getMessage();
+}
+
+?>
+
